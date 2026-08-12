e30a6c7ed917f02874814a201a167fbfd82e880f
Morcman01 <markjhonson257@gmail.com>
2026-08-12 21:28:42 +0700

    added basic user login and session authentication (no UI)

---
+++ includes/auth.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+
+function isLoggedIn(): bool
+{
+    return isset($_SESSION['user_id']);
+}
+
+function requireLogin(): void
+{
+    if(!isLoggedIn()){
+        http_response_code(401);
+
+        echo json_encode([
+            'success' => false,
+            'message' => "Authentication required"
+        ]);
+
+        exit;
+    }
+}
+
+function requireUploader(): void
+{
+    requireLogin();
+
+    if($_SESSION['role'] !== 'uploader'){
+        http_response_code(403);
+
+        echo json_encode([
+        'success' => false,
+        'message' => "Uploader permission required"
+    ]);
+
+    exit;
+    }
+}
+
+?>
---
+++ public/login.php
@@ -0,0 +1,80 @@
+<?php
+session_start();
+
+require __DIR__ . '/../includes/db.php';
+
+header("Content-Type: application/json");
+
+//get json data from javascript
+$data = json_decode(file_get_contents('php://input'), true);
+
+if(!isset($data['username']) || !isset($data['password'])){
+    http_response_code(400);
+
+    echo json_encode([
+        'success' => false,
+        'message' => "Username and password required"
+    ]);
+    
+    exit;
+}
+
+$username = $data['username'];
+$password = $data['password'];
+
+
+//pull user data from database using given username
+$sql = "SELECT id, username, password_hash, role
+        FROM users
+        WHERE username = ?";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $username);
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+
+//if user doesn't exist
+if($result->num_rows === 0){
+    http_response_code(400);
+
+    echo json_encode([
+        'success' => false,
+        'message' => "Invalid username or password"
+    ]);
+
+    exit;
+}
+
+//get user data from database as associate array
+$user = $result -> fetch_assoc();
+
+//check password
+if(password_verify($password, $user['password_hash'])){
+    http_response_code(401);
+
+    echo json_encode([
+        'success' => false,
+        'message' => "Invalid username or password"
+    ]);
+
+    exit;
+}
+
+//auth successful
+session_regenerate_id(true);
+
+$_SESSION['user_id'] = $user['id'];
+$_SESSION['username'] = $user['username'];
+$_SESSION['role'] = $user['role'];
+
+echo json_encode([
+    'success' => true,
+    'message' => "Login successful, hey there " . $username
+]);
+
+exit;
+
+
+?>
